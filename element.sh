fe3324cee0740e8d67b223e4181be841f54b8ae9
Tyler Kortge <65042202+GameScientist@users.noreply.github.com>
2023-03-23 11:33:10 -0400

    Update element.sh

--- element.sh
+++ element.sh
@@ -20,7 +20,7 @@ else
       echo $($PSQL "SELECT atomic_mass, melting_point_celsius, boiling_point_celsius, type_id FROM properties WHERE atomic_number='$ATOMIC_NUMBER'") | while IFS="|" read MASS MELT BOIL TYPE_ID
       do
         TYPE=$($PSQL "SELECT type FROM types WHERE type_id=$TYPE_ID") 
-        echo "The element with atomic number "$ATOMIC_NUMBER" is "$NAME" ("$SYMBOL"). It's a "$TYPE", with a mass of "$MASS" amu. "$NAME" has a melting point of "$MELT" celsius and a boiling point of "$BOIL" celsius."
+        echo -e "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELT celsius and a boiling point of $BOIL celsius."
       done
     fi
   done
